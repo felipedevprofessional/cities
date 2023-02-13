7a68fd5472025919e9d0f2db031429e11c2a1b8c
Felipe de Jesus Oliveira Ribeiro <felipe.j.ribeiro@basis.com.br>
2023-02-13 18:14:48 -0300

    first commit

---
+++ src/test/java/com/github/dejesus26/cities/CitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dejesus26.cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
